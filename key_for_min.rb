635d91379769c20db90e5d553f6afaed1b6a1948
Micah Mosley <micah.mosley@utexas.edu>
2020-06-28 19:58:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash==nil 
+    return nil 
+  end 
   lowest=1000000
   key="this"
   name_hash.each do |item, price|
     if price<lowest
+      lowest=price
       key=item
     end
   end 
